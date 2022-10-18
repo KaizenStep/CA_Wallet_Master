42d1c9003e836bd281f85cc3a41e56fde0b4b27e
KaizenStep Cloud Solutions <48591828+KaizenStep@users.noreply.github.com>
2022-10-18 15:56:25 +0200

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -637,7 +637,7 @@ define([
         } else if (Method == 'Update') {
 
             if (StepActual == '5') {
-                if (($('.updatepass input[type=text]').val() === 'Undefined' || $('.updatepass input[type=text]').val().length === 0) || ($('.updatepass select').find('option:selected').attr('value').trim() === 'Undefined' || $('.updatepass select').find('option:selected').attr('value').trim().length === 0)) {
+                if (/*($('.updatepass input[type=text]').val() === 'Undefined' || $('.updatepass input[type=text]').val().length === 0) || */($('.updatepass select').find('option:selected').attr('value').trim() === 'Undefined' || $('.updatepass select').find('option:selected').attr('value').trim().length === 0)) {
                     FlagFields(Method);
                 } else {
                     WriteSummary(Method);
